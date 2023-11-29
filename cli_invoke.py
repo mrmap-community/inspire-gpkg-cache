f6a77e56a707dc50184d76a8453440919375650b
armin11 <armin.retterath@googlemail.com>
2023-11-29 09:49:30 +0100

    Add further method to cli invocation

--- cli_invoke.py
+++ cli_invoke.py
@@ -7,11 +7,17 @@ print(sys.argv[1])
 
 configuration = json.loads(sys.argv[1])
 
-cache = SpatialDataCache(configuration['dataset_configuration'], configuration['area_of_interest'], 'https://vocabulary.geoportal.rlp.de/geonetwork/srv/ger/csw')
-
-json_result = cache.check_options()
-
-print(json_result)
+if sys.argv[2] == 'checkOptions':
+    cache = SpatialDataCache(configuration['dataset_configuration'], json.dumps(configuration['area_of_interest']), ['https://vocabulary.geoportal.rlp.de/geonetwork/srv/ger/csw'])
+    json_result = cache.check_options()
+    # give back json with download options
+    print(json_result)
+    sys.exit()
+if sys.argv[2] == 'generateCache':
+    cache = SpatialDataCache(configuration['dataset_configuration'], json.dumps(configuration['area_of_interest']), ['https://vocabulary.geoportal.rlp.de/geonetwork/srv/ger/csw'], output_filename='test', output_folder='/home/armin/GDI-RP/devel/Geoportal/Mapbender2.8/http/tmp/')
+    print('start generate cache')
+    cache.generate_cache()
+    sys.exit()
 
 
 
